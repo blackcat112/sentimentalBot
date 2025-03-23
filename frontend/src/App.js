a5abc54e0236847ecac5d16a343656352cbccf41
blackCat <103961499+blackcat112@users.noreply.github.com>
2025-03-23 20:27:52 +0200

    newfrontend commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
       });
       setSentiment(response.data.sentiment);
     } catch (error) {
-      console.error("Error al obtener el sentimiento", error);
+      console.error("Error", error);
     } finally {
       setLoading(false);
     }
@@ -25,23 +25,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Análisis de Sentimientos</h1>
+        <h1>Sentiment Anylyst</h1>
       </header>
       <main className="App-main">
         <form onSubmit={handleSubmit} className="sentiment-form">
           <textarea
             value={text}
             onChange={(e) => setText(e.target.value)}
-            placeholder="Escribe tu texto aquí"
+            placeholder="Writte here ..."
             className="sentiment-textarea"
           />
           <button type="submit" disabled={loading} className="sentiment-button">
-            {loading ? "Analizando..." : "Analizar Sentimiento"}
+            {loading ? "Analysing..." : "Analyse"}
           </button>
         </form>
         {sentiment && (
           <div className="result">
-            <h2>Resultado:</h2>
+            <h2>Sentiment:</h2>
             <p className={`sentiment ${sentiment.toLowerCase()}`}>{sentiment}</p>
           </div>
         )}
