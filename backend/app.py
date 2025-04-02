c6d00b1bf58dee736660436f40e5fa9af4a2aaeb
blackCat <103961499+blackcat112@users.noreply.github.com>
2025-04-02 21:24:09 +0300

    backend2.1

--- backend/app.py
+++ backend/app.py
@@ -65,5 +65,5 @@ def predict():
         return jsonify({"error": f"Error en la predicción: {e}"}), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
